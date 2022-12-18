a0e82f1b9e47e025c03227fe28e55a03b170e00e
caleigh <caleighsteill@outlook.com>
2022-12-18 15:30:47 -0700

    commit tree a39b2cb4d862db966031ae350b9fc7ec4f625743

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+
+function superbowlWin(record) {
+    const result = record.find(record => record.result === "W")
+    return result ? result.year : undefined
+}
+
+
+// function superbowlWin(record) {
+//     for (const year of record) {
+//         if (record[year] === "W") {
+//             return record[year]
+//         } else {
+//             return "undefined"
+//         }
+//     }
+// }
+
+// record.find(superbowlWin)
